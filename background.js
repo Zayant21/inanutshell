3927d64e21f07cd7cd806acfe72bf0d6826b5584
Zayan Tofeeq <zayantofeeq@csu.fullerton.edu>
2023-04-23 05:24:21 -0700

    added API_SUPPORT

--- background.js
+++ background.js
@@ -1,60 +1,60 @@
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+const dotenv = require('dotenv');
+dotenv.config(); // load environment variables from .env file
+const apiKey = process.env.API_KEY; // access API key from environment variable
+ 
+ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.enabled === true) {
-  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete') {
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        var activeTab = tabs[0];
-        if (activeTab && activeTab.url && activeTab.url.indexOf('http') == 0) {
-          chrome.tabs.executeScript(null, {file: 'content.js'}, function() {
-            chrome.tabs.sendMessage(activeTab.id, {text: 'get_content'}, function(response) {
-              if (response) {
-                console.log(response)
-              }
-            });
-          });
-        }
-      });
-    }
-  });
+//   chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+//     if (changeInfo.status == 'complete') {
+//       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//         var activeTab = tabs[0];
+//         if (activeTab && activeTab.url && activeTab.url.indexOf('http') == 0) {
+//           chrome.tabs.executeScript(null, {file: 'content.js'}, function() {
+//             chrome.tabs.sendMessage(activeTab.id, {text: 'get_content'}, function(response) {
+//               if (response) {
+//                 console.log(response)
+//               }
+//             });
+//           });
+//         }
+//       });
+//     }
+//   });
 
   chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     console.log(message.command)
     if (message.text == 'help_s') {
       const command = message.command;
-      const url = 'https://api.openai.com/v1/engines/davinci-codex/completions';
-      const data = {
-        'prompt': `A short explanation of ${command}`,
-        'max_tokens': 60,
-        'temperature': 0.3,
-        'stop': null
-      };
+      const url = 'https://api.openai.com/v1/chat/completions';
+      const GPTresponse = {
+        'model': 'gpt-3.5-turbo',
+        'messages': [{'role': 'user', 'content': `${command} `}],
+       'max_tokens': 50,
+       'temperature': 0.2,
+       'stop': null
+     };
       const headers = {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer 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`,
+        'Authorization': `Bearer ${current.env.API_KEY}`,
       };
   
       fetch(url, {
         method: 'POST',
         headers: headers,
-        body: JSON.stringify(data)
+        body: JSON.stringify(GPTresponse)
       })
       .then(response => response.json())
-      .then(data => {
-        const text = data.choices[0].text.trim();
-        console.log(text);
+      .then(GPTresponse => {
+        const text = GPTresponse.choices[0].message.content.trim();
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
           chrome.tabs.sendMessage(tabs[0].id, {text: 'update_input', value: text});
         });
       })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
+      .catch(error => console.log(error));
 
     }
     else if (message.text == 'help') {
       const command = message.command;
-      console.log('1st')
       let responses = `An explanation of ${command}`;
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {text: 'update_input', value: responses});
@@ -64,17 +64,17 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
 
       // Add a click event listener to the "Summarize" button
-  document.getElementById('summarize').addEventListener('click', function() {
-        // Send a message to the content script to summarize the page
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      var activeTab = tabs[0];
-      chrome.tabs.sendMessage(activeTab.id, {text: 'summarize'});
-    });
-  });
-  }
+  // document.getElementById('summarize').addEventListener('click', function() {
+  //       // Send a message to the content script to summarize the page
+  //   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  //     var activeTab = tabs[0];
+  //     chrome.tabs.sendMessage(activeTab.id, {text: 'summarize'});
+  //   });
+  // });
+   }
 
-else {
-  console.log("Toggle is off")
-}
+ else {
+   console.log("Toggle is off")
+ }
 
-});
+ });
--- content.js
+++ content.js
@@ -1,49 +1,96 @@
-const nutshell = /\$nutshell\s+([^;]+)\s*;/;
+const nutshell = /\$(nutshell|ns)\s+([^;]+)\s*;/
 const nutshell_s = /\$nutshell\s+-s\s+([^;]+)\s*;/;
 
-
-document.addEventListener("input", function(event) {
-  if (event.target.value.startsWith('$nutshell')) {
-    const match_s = event.target.value.match(nutshell_s);
-    const match = event.target.value.match(nutshell);
-    if (match_s) {
-      const command = match_s[1].trim();
-      console.log(`Command: ${command}`);
-      chrome.runtime.sendMessage({text: 'help_s',command: command }, function(response) {
-      });
-    } else if (match) {
-      const command = match[1].trim();
-      console.log(`Command: ${command}`);
-      chrome.runtime.sendMessage({text: 'help',command: command }, function(response) {
-
-      });
+  document.addEventListener("input", function(event) {
+    if (event.target.textContent.includes('$nutshell')) {
+      const match_s = event.target.textContent.match(nutshell_s);
+      const match = event.target.textContent.match(nutshell);
+      if (match_s) {
+        const command = match_s[1].trim();
+        console.log(`Command: ${command}`);
+        chrome.runtime.sendMessage({text: 'help_s',command: command }, function(response) {
+        });
+      } else if (match) {
+        const command = match[1].trim();
+        console.log(`Command: ${command}`);
+        chrome.runtime.sendMessage({text: 'help',command: command }, function(response) {
+  
+        });
+      }
+      else {
+        console.error('Extension context is invalid');
+      }
     }
-    else {
-      console.error('Extension context is invalid');
-    }
-  }
+  });
+
+
+// if (document.activeElement.tagName === 'INPUT'){
+
+//   document.addEventListener("input", function(event) {
+//     if (event.target.value.startsWith('$nutshell')) {
+//       const match_s = event.target.value.match(nutshell_s);
+//       const match = event.target.value.match(nutshell);
+//       if (match_s) {
+//         const command = match_s[1].trim();
+//         console.log(`Command: ${command}`);
+//         chrome.runtime.sendMessage({text: 'help_s',command: command }, function(response) {
+//         });
+//       } else if (match) {
+//         const command = match[1].trim();
+//         console.log(`Command: ${command}`);
+//         chrome.runtime.sendMessage({text: 'help',command: command }, function(response) {
+
+//         });
+//       }
+//       else {
+//         console.error('Extension context is invalid');
+//       }
+//     }
+//   });
+// }
+
+
+
+  chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.text == 'update_input') {
+      document.activeElement.textContent += message.value;
+}
 });
 
+// if (document.activeElement.tagName === 'INPUT'){  
+// chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+//   if (message.text == 'update_input') {
+//     document.activeElement.value = message.value;
+//   }
+// });
+// }
 
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if (message.text == 'update_input') {
-    document.activeElement.value = message.value;
-  }
-});
+// chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+//   if (message.text == 'get_content') {
+//     var content = document.body.innerText;
+//     sendResponse({content: content});
+//     console.log('2nd message being sent')
+//   }
+// });
 
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if (message.text == 'get_content') {
-    var content = document.body.innerText;
-    sendResponse({content: content});
-    console.log('2nd message being sent')
-  }
-});
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if (message.text == 'summarize') {
-    console.log('got summarize content js')
-  }
-});
+// chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+//   if (message.text == 'summarize') {
+//     console.log('got summarize content js')
+//   }
+// });
 
 
+// var iframes = document.querySelectorAll('iframe');
+// var iframedoc, iframedocbody;
+
+// if (iframes.length > 0) {
+//   for (let iframe of iframes) {
+//     if (iframedoc = (iframe.contentDocument || iframe.contentWindow.document)) {
+//       const bodyText = iframedoc.querySelector('body');
+//       if (bodyText.innerText !== undefined) {
+//         iframedocbody = bodyText;
+//       }
+//     }
+//   }
