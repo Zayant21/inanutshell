d0e61feb5c887cdf779e6891ba60e4915c6d4744
Zayan Tofeeq <zayantofeeq@csu.fullerton.edu>
2023-04-28 04:36:31 -0700

    -

--- reg ex.js
+++
@@ -1,6 +0,0 @@
-const man_nutshell = /^\::man ns;$/;
-const nutshell = /\::ns\s+([^;-]+)\s*;/;
-const nutshell_s = /\::ns\s+-s\s+([^;]+)\s*;/;
-const nutshell_lc = /^\::ns\s+-lc\s+([^;]+);\s*$/ ;
-const nutshell_sum = /^\::ns\s+-sum;\s*$/;
-const nutshell_version = /^\::ns\s+-v;\s*$/;
